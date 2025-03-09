6a29ade952d77d9bda44b0b31489d9319b2e6329
Danilo-MartinsM <danilo.martins.dev@gmail.com>
2025-03-09 09:37:02 -0300

    teste1

--- teste1.c
+++ teste1.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main () {
     
-    int x;
+    int idade;
+    double salario, altura;
+    char genero;
+    char nome [50];
 
-    x = 10;
+    idade = 20;
+    salario = 5800.5;
+    altura = 1.63;
+    genero = 'F';
+    strcpy(nome, "Maria Silva");
 
-    printf("%d", x);
+    printf("IDADE = %d\n", idade);
+    printf("SALARIO = %.2lf\n", salario);
+    printf("ALTURA = %.2lf\n", altura);
+    printf("GENERO = %c\n", genero);
+    printf("NOME = %s\n", nome);
 
     return 0;
 }
